5daa6be2def8887e1e20714c43185e5c9d9d2910
Anandhu <connect.anandhukannan@gmail.com>
2026-01-08 22:39:08 +0530

    added cmp

---
+++ cmp.s
@@ -0,0 +1,22 @@
+//program for negative
+.global _start
+_start:
+	mov r1, #2
+	mov r2, #4
+	cmp r1, r2
+	
+
+//program for zero 
+.global _start
+_start:
+	mov r1, #4
+	mov r2, #4
+	cmp r1, r2
+
+
+//program for +ve 
+.global _start
+_start:
+	mov r1, #4
+	mov r2, #2
+	cmp r1, r2
